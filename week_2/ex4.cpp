036263366914e3136c8ff261c0b5198f0fc9a92d
Bui Anh Quoc <p.lyquangk.lythedan@gmail.com>
2024-03-19 08:44:52 +0700

    BCA

--- week_2/ex4.cpp
+++ week_2/ex4.cpp
@@ -3,14 +3,14 @@
 
 using namespace std;
 
-bool** conflict;
-int *load, m, n, *teacher;
-unsigned int record = -1;
-unordered_map<int, bool>* preferences;
+bool conflict[31][31];
+int m, n, teacher[31];
+unsigned int load[11], record = -1;
+unordered_map<int, bool> preferences[31];
 
 bool check(int c, int t) {
-    // If the course is in the prefer list of the teacher
-    if (!preferences[t][c])
+    // If the course isn't in the prefer list of the teacher
+    if (!preferences[c][t])
     {
         return false;
     }
@@ -24,7 +24,7 @@ bool check(int c, int t) {
     // Try to find conflict
     for (int i = 1; i < c; i++)
     {
-        if (teacher[i] == t && conflict[i][c])
+        if (teacher[i] == t && (conflict[i][c] || conflict[c][i]))
         {
             return false; // If found conflict
         }
@@ -34,7 +34,7 @@ bool check(int c, int t) {
 
 void update() {
     record = load[1];
-    for (int i = 2; i <= n; i++)
+    for (int i = 2; i <= m; i++)
     {
         if (load[i] > record)
         {
@@ -44,9 +44,15 @@ void update() {
 }
 
 // (course: k)
-void attempt(int k) {    
+void attempt(int k) {
+    if (k == n + 1)
+    {
+        update();
+        return;
+    }
+    
     // Browse all teacher (teacher: i)
-    for (int i = 0; i < m; i++)
+    for (int i = 1; i <= m; i++)
     {
         if (check(k, i))
         {
@@ -55,17 +61,7 @@ void attempt(int k) {
             ++load[i];
 
             // Load condition has been checked in check()
-            // attempt(k + 1);
-            if (k == n)
-            {
-                update();
-            }
-            else {
-                if (load[i] < record)
-                {
-                    attempt(k + 1);
-                }
-            }
+            attempt(k + 1);
 
             // Restore
             --load[i];
@@ -76,64 +72,35 @@ void attempt(int k) {
 int main() {
     freopen("input.txt", "r", stdin);
 
-    int k, k1, k2;
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int k, k1, k2, tmp, _tmp;
 
     cin >> m >> n;
     
-    conflict = new bool*[n + 1];
-    for (int i = 1; i <= n; i++)
-    {
-        conflict[i] = new bool[n + 1];
-        for (int j = 1; j <= n; j++)
-        {
-            conflict[i][j] = false; // Not conflict -> false
-        }
-    }
-
-    load = new int[m];
-    for (int i = 0; i < m; i++)
-    {
-        load[i] = 0;
-    }
-    
     // Initialize preferences
-    preferences = new unordered_map<int, bool>[m];
-    for (int i = 0; i < m; i++)
+    for (int i = 1; i <= m; i++)
     {
-        int tmp, _tmp;
-
         cin >> tmp;
 
         for (int j = 0; j < tmp; j++)
         {
             cin >> _tmp;
-            preferences[i][_tmp] = true;
+            preferences[_tmp][i] = true;
         }
     }
-
-    teacher = new int[n + 1];
 
     cin >> k;
     for (int i = 0; i < k; i++)
     {
         cin >> k1 >> k2;
         conflict[k1][k2] = true; // Conflict -> true
-        // conflict[k2][k1] = true; // Conflict -> true
+        conflict[k2][k1] = true; // Conflict -> true
     }
-
-    // for (int i = 1; i <= n; i++)
-    // {
-    //     for (int j = 1; j <= n; j++)
-    //     {
-    //         cout << conflict[i][j] << ' ';
-    //     }
-    //     cout << endl;
-    // }
-    
 
     attempt(1);
 
-    cout << (int)record;
+    cout << record;
 
     return 0;
 }
